36df140334030076cab830c21f2bea7a8b2a5753
Alex Andrade <g.alex.andrade@gmail.com>
2020-09-15 18:07:24 -0600

    Creating example

---
+++ build/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var ImageMarker = function ImageMarker() {
+  return /*#__PURE__*/_react["default"].createElement("p", null, "Image Marker");
+};
+
+var _default = ImageMarker;
+exports["default"] = _default;
---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ImageMarker from "react-image-marker";
+
+function App() {
+  return <ImageMarker />;
+}
+
+export default App;
